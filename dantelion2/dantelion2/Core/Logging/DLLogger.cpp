932aad426796b0ebf0d8760fd3e99527812a3b0c
Radai <lordradai@null.net>
2026-03-01 02:13:14 +0100

    Update DLLogger.cpp

--- dantelion2/dantelion2/Core/Logging/DLLogger.cpp
+++ dantelion2/dantelion2/Core/Logging/DLLogger.cpp
@@ -3,9 +3,9 @@
 
 namespace DLLG
 {
-	typedef void(_fastcall* oLog)(DLLogger*, DLLogLevel, const dl_wchar*, ...);
-	typedef void(_fastcall* oLogSpecificW)(DLLogger*, const dl_wchar*, ...);
-	typedef void(_fastcall* oLogSpecificA)(DLLogger*, const dl_char*, ...);
+	typedef void(_cdecl* oLog)(DLLogger*, DLLogLevel, const dl_wchar*, ...);
+	typedef void(_cdecl* oLogSpecificW)(DLLogger*, const dl_wchar*, ...);
+	typedef void(_cdecl* oLogSpecificA)(DLLogger*, const dl_char*, ...);
 
 	DLLogger::DLLogger(const dl_wchar* name)
 		: m_cName(name), m_level(DLLogLevel::LL_INTEGER_DEBUG), m_iVar14(0)
@@ -20,22 +20,58 @@ namespace DLLG
 
 	void DLLogger::Log(DLLogLevel level, const dl_wchar* message, ...)
 	{
-		CALL(oLog, 0x8819b0, this, level, message);
+		va_list args;
+		va_start(args, message);
+
+		// Format the string into a local buffer
+		wchar_t buffer[1024];
+		_vsnwprintf_s(buffer, _TRUNCATE, message, args);
+
+		va_end(args);
+
+		CALL(oLog, 0x8819b0, this, level, buffer);
 	}
 
 	void DLLogger::LogDebug(const dl_wchar* message, ...)
 	{
-		CALL(oLogSpecificW, 0x881a00, this, message);
+		va_list args;
+		va_start(args, message);
+
+		// Format the string into a local buffer
+		wchar_t buffer[1024];
+		_vsnwprintf_s(buffer, _TRUNCATE, message, args);
+
+		va_end(args);
+
+		CALL(oLogSpecificW, 0x881a00, this, buffer);
 	}
 
 	void DLLogger::LogDebug(const dl_char* message, ...)
 	{
-		CALL(oLogSpecificA, 0x881c40, this, message);
+		va_list args;
+		va_start(args, message);
+
+		// Format the string into a local buffer
+		char buffer[1024];
+		vsprintf_s(buffer, message, args);
+
+		va_end(args);
+
+		CALL(oLogSpecificA, 0x881c40, this, buffer);
 	}
 
 	void DLLogger::LogInfo(const dl_wchar* message, ...)
 	{
-		CALL(oLogSpecificW, 0x881a50, this, message);
+		va_list args;
+		va_start(args, message);
+
+		// Format the string into a local buffer
+		wchar_t buffer[1024];
+		_vsnwprintf_s(buffer, _TRUNCATE, message, args);
+
+		va_end(args);
+
+		CALL(oLogSpecificW, 0x881a50, this, buffer);
 	}
 
 	void DLLogger::LogInfo(const dl_char* message, ...)
@@ -45,7 +81,16 @@ namespace DLLG
 
 	void DLLogger::LogNotice(const dl_wchar* message, ...)
 	{
-		CALL(oLogSpecificW, 0x881aa0, this, message);
+		va_list args;
+		va_start(args, message);
+
+		// Format the string into a local buffer
+		wchar_t buffer[1024];
+		_vsnwprintf_s(buffer, _TRUNCATE, message, args);
+
+		va_end(args);
+
+		CALL(oLogSpecificW, 0x881aa0, this, buffer);
 	}
 
 	void DLLogger::LogNotice(const dl_char* message, ...)
@@ -55,7 +100,16 @@ namespace DLLG
 
 	void DLLogger::LogWarn(const dl_wchar* message, ...)
 	{
-		CALL(oLogSpecificW, 0x881af0, this, message);
+		va_list args;
+		va_start(args, message);
+
+		// Format the string into a local buffer
+		wchar_t buffer[1024];
+		_vsnwprintf_s(buffer, _TRUNCATE, message, args);
+
+		va_end(args);
+
+		CALL(oLogSpecificW, 0x881af0, this, buffer);
 	}
 
 	void DLLogger::LogWarn(const dl_char* message, ...)
@@ -65,7 +119,16 @@ namespace DLLG
 
 	void DLLogger::LogError(const dl_wchar* message, ...)
 	{
-		CALL(oLogSpecificW, 0x881b40, this, message);
+		va_list args;
+		va_start(args, message);
+
+		// Format the string into a local buffer
+		wchar_t buffer[1024];
+		_vsnwprintf_s(buffer, _TRUNCATE, message, args);
+
+		va_end(args);
+
+		CALL(oLogSpecificW, 0x881b40, this, buffer);
 	}
 
 	void DLLogger::LogError(const dl_char* message, ...)
@@ -75,7 +138,16 @@ namespace DLLG
 
 	void DLLogger::LogFatal(const dl_wchar* message, ...)
 	{
-		CALL(oLogSpecificW, 0x881b90, this, message);
+		va_list args;
+		va_start(args, message);
+
+		// Format the string into a local buffer
+		wchar_t buffer[1024];
+		_vsnwprintf_s(buffer, _TRUNCATE, message, args);
+
+		va_end(args);
+
+		CALL(oLogSpecificW, 0x881b90, this, buffer);
 	}
 
 	void DLLogger::LogFatal(const dl_char* message, ...)
