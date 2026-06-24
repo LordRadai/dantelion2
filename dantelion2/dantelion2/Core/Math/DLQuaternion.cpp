18ac4ddd2d0e9171048157a625e37b3bfbd74ed9
Radai <lordradai@null.net>
2026-06-24 12:01:00 +0200

    fix

--- dantelion2/dantelion2/Core/Math/DLQuaternion.cpp
+++ dantelion2/dantelion2/Core/Math/DLQuaternion.cpp
@@ -11,7 +11,7 @@ namespace DLMT
     namespace
     {
         template<typename T>
-        constexpr const T& clamp(const T& v, const T& lo, const T& hi) {
+        const T& clamp(const T& v, const T& lo, const T& hi) {
             return (v < lo) ? lo : (hi < v) ? hi : v;
         }
     }
--- dantelion2/dantelion2/Core/Math/DLVector.cpp
+++ dantelion2/dantelion2/Core/Math/DLVector.cpp
@@ -9,7 +9,7 @@ namespace DLMT
     namespace
     {
         template<typename T>
-        constexpr const T& clamp(const T& v, const T& lo, const T& hi) {
+        const T& clamp(const T& v, const T& lo, const T& hi) {
             return (v < lo) ? lo : (hi < v) ? hi : v;
         }
     }
