3d9bd3bff430ce545cc4b3dd49c1eae4f237d8da
Radai <lordradai@null.net>
2026-06-25 21:06:06 +0200

    fix

--- dantelion2/dantelion2/Core/System/DLAssertEventManager.cpp
+++ dantelion2/dantelion2/Core/System/DLAssertEventManager.cpp
@@ -39,7 +39,9 @@ namespace DLSY
 
 	void DLAssertEventManagerImpl::FireAssertEvent(const DLPF::DLAssertEvent& event)
 	{
+#ifdef _DEV
 		_wassert(event.Msg, event.File, event.Line);
+#endif
 		//CALL(FireAssertEvent_t, 0x88ea50, this, event);
 	}
 
