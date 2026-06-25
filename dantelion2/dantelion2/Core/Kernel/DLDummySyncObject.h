93651d03de014444477249a11e24d2f95ee11219
Radai <lordradai@null.net>
2026-06-25 17:16:05 +0200

    Update DLDummySyncObject.h

--- dantelion2/dantelion2/Core/Kernel/DLDummySyncObject.h
+++ dantelion2/dantelion2/Core/Kernel/DLDummySyncObject.h
@@ -8,5 +8,10 @@ namespace DLKR
 {
 	class DLDummySyncObject : public DLSyncObject
 	{
+	public:
+		virtual dl_bool IsValid(void) const override { return true; }
+		virtual dl_int32 Lock(DLTimeout timeout = DL_TIMEOUT_INFINITE) override { return 0; }
+		virtual dl_int32 TryLock(void) override { return 0; }
+		virtual dl_int32 Unlock(void) override { return 0; }
 	};
 }
