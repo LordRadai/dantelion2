ce6d5e7da682762a5317448816aaa8a48dda6b18
Radai <lordradai@null.net>
2026-06-29 12:40:31 +0200

    fix

--- dantelion2/dantelion2/Core/Graphics/DLVertexTypes.h
+++ dantelion2/dantelion2/Core/Graphics/DLVertexTypes.h
@@ -6,8 +6,8 @@ namespace DLGR
 	class DLVertexPositionColor
 	{
 	public:
-		DLMT::DL_VECTOR3 m_position = DLMT::DL_VECTOR3_ZERO;
-		DLMT::DL_VECTOR4 m_color = DLMT::DL_VECTOR4_ZERO;
+		DLMT::DL_VECTOR3AL m_position = DLMT::DL_VECTOR3_ZERO;
+		DLMT::DL_COLOR_F4 m_color = DLMT::DL_VECTOR4_ZERO;
 
 		DLVertexPositionColor() {}
 		DLVertexPositionColor(DLMT::DL_VECTOR3 position, DLMT::DL_COLOR_F4 color);
@@ -19,7 +19,7 @@ namespace DLGR
 	{
 	public:
 		DLMT::DL_VECTOR3AL m_position = DLMT::DL_VECTOR3_ZERO;
-		DLMT::DL_VECTOR4AL m_color = DLMT::DL_VECTOR4_ZERO;
+		DLMT::DL_COLOR_F4 m_color = DLMT::DL_VECTOR4_ZERO;
 		DLMT::DL_VECTOR3AL m_normal = DLMT::DL_VECTOR3_UNITZ;
 
 		DLVertexPositionColorNormal() {}
