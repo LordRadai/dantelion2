8953953053c0f91c43b4fd3e9c15c3760b2d10c1
Radai <lordradai@null.net>
2026-06-30 21:37:46 +0200

    Update DLArrow.h

--- dantelion2/dantelion2/Core/LinearEx/DLArrow.h
+++ dantelion2/dantelion2/Core/LinearEx/DLArrow.h
@@ -7,10 +7,10 @@ namespace DLLinearEx
 	{
 	public:
 		DL_ARROW() = default;
-		DL_ARROW(const DLMT::DL_SEGMENT& segment, dl_float32 shaftLength, dl_float32 headRadius, dl_float32 shaftRadius)
-			: m_Segment(segment), m_fShaftLength(shaftLength), m_fHeadRadius(headRadius), m_fShaftRadius(shaftRadius) {}
-		DL_ARROW(const DLMT::DL_VECTOR4AL& origin, const DLMT::DL_VECTOR4AL& direction, dl_float32 shaftLength, dl_float32 headRadius, dl_float32 shaftRadius)
-			: m_Segment(origin, direction), m_fShaftLength(shaftLength), m_fHeadRadius(headRadius), m_fShaftRadius(shaftRadius) {}
+		DL_ARROW(const DLMT::DL_SEGMENT& segment, dl_float32 shaftRadius, dl_float32 headRadius, dl_float32 shaftRatio)
+			: m_Segment(segment), m_fShaftRadius(shaftRadius), m_fHeadRadius(headRadius), m_fShaftRatio(shaftRatio) {}
+		DL_ARROW(const DLMT::DL_VECTOR4AL& origin, const DLMT::DL_VECTOR4AL& direction, dl_float32 shaftRadius, dl_float32 headRadius, dl_float32 shaftRatio)
+			: m_Segment(origin, direction), m_fShaftRadius(shaftRadius), m_fHeadRadius(headRadius), m_fShaftRatio(shaftRatio) {}
 		
 		DLMT::DL_VECTOR4AL GetOrigin() const { return m_Segment.GetOrigin(); }
 		DLMT::DL_VECTOR4AL GetDirection() const { return m_Segment.GetDirection(); }
@@ -18,13 +18,13 @@ namespace DLLinearEx
 		DLMT::DL_VECTOR4AL GetCenter() const { return m_Segment.GetCenter(); }
 		dl_float32 GetLength() const { return m_Segment.GetLength(); }
 		dl_float32 GetLengthSq() const { return m_Segment.GetLengthSq(); }
-		dl_float32 GetShaftLength() const { return m_fShaftLength; }
-		dl_float32 GetHeadRadius() const { return m_fHeadRadius; }
 		dl_float32 GetShaftRadius() const { return m_fShaftRadius; }
+		dl_float32 GetHeadRadius() const { return m_fHeadRadius; }
+		dl_float32 GetShaftLengthRatio() const { return m_fShaftRatio; }
 	private:
 		DLMT::DL_SEGMENT m_Segment;
-		dl_float32 m_fShaftLength;
-		dl_float32 m_fHeadRadius;
 		dl_float32 m_fShaftRadius;
+		dl_float32 m_fHeadRadius;
+		dl_float32 m_fShaftRatio;
 	};
 }
