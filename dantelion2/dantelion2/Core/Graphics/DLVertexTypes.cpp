2be3d80e917a9da1161a73e2364d34f3b5729d09
Radai <lordradai@null.net>
2026-07-02 12:56:09 +0200

    fix

--- dantelion2/dantelion2/Core/Graphics/DLVertexTypes.cpp
+++ dantelion2/dantelion2/Core/Graphics/DLVertexTypes.cpp
@@ -3,20 +3,20 @@
 namespace DLGR
 {
 	DLVertexPositionColor::DLVertexPositionColor(DLMT::DL_VECTOR3 position, DLMT::DL_COLOR_F4 color)
-		: m_position(position.x, position.y, position.z), m_color(color) {}
+		: Position(position.x, position.y, position.z), Color(color) {}
 
 	DLVertexPositionColor::DLVertexPositionColor(DLMT::DL_VECTOR3 position, DLMT::DL_COLOR_32 color)
-		: m_position(position.x, position.y, position.z), m_color(static_cast<DLMT::DL_COLOR_F4>(color)) {}
+		: Position(position.x, position.y, position.z), Color(static_cast<DLMT::DL_COLOR_F4>(color)) {}
 
 	DLVertexPositionColor::DLVertexPositionColor(DLMT::DL_VECTOR3 position, DLMT2::DL_COLOR_U8 color)
-		: m_position(position.x, position.y, position.z), m_color(static_cast<DLMT::DL_COLOR_F4>(color)) {}
+		: Position(position.x, position.y, position.z), Color(static_cast<DLMT::DL_COLOR_F4>(color)) {}
 
 	DLVertexPositionColorNormal::DLVertexPositionColorNormal(DLMT::DL_VECTOR3 position, DLMT::DL_COLOR_F4 color, DLMT::DL_VECTOR3 normal)
-		: m_position(position.x, position.y, position.z), m_color(color), m_normal(normal.x, normal.y, normal.z) {}
+		: Position(position.x, position.y, position.z), Color(color), Normal(normal.x, normal.y, normal.z) {}
 
 	DLVertexPositionColorNormal::DLVertexPositionColorNormal(DLMT::DL_VECTOR3 position, DLMT::DL_COLOR_32 color, DLMT::DL_VECTOR3 normal)
-		: m_position(position.x, position.y, position.z), m_color(static_cast<DLMT::DL_COLOR_F4>(color)), m_normal(normal.x, normal.y, normal.z) {}
+		: Position(position.x, position.y, position.z), Color(static_cast<DLMT::DL_COLOR_F4>(color)), Normal(normal.x, normal.y, normal.z) {}
 
 	DLVertexPositionColorNormal::DLVertexPositionColorNormal(DLMT::DL_VECTOR3 position, DLMT2::DL_COLOR_U8 color, DLMT::DL_VECTOR3 normal)
-		: m_position(position.x, position.y, position.z), m_color(static_cast<DLMT::DL_COLOR_F4>(color)), m_normal(normal.x, normal.y, normal.z) {}
+		: Position(position.x, position.y, position.z), Color(static_cast<DLMT::DL_COLOR_F4>(color)), Normal(normal.x, normal.y, normal.z) {}
 }
