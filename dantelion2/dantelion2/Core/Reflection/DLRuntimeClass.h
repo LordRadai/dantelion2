09eb1055d953eb3ac5113d1012d961ea5db5e931
Radai <lordradai@null.net>
2026-08-20 20:42:11 +0200

    DLRuntimeClass interface fix

--- dantelion2/dantelion2/Core/Reflection/DLRuntimeClass.h
+++ dantelion2/dantelion2/Core/Reflection/DLRuntimeClass.h
@@ -10,7 +10,6 @@ namespace DLRF
 
 	class DLRuntimeClass
 	{
-		void** _vfptr;
 	public:
         template<class T>
         struct NamedInfo 
@@ -31,13 +30,36 @@ namespace DLRF
 		typedef DLUT::DLVector<TypedInfo> TypeInfoList;
 		typedef DLUT::DLVector<NamedInfo<DLMethod>> MethodList;
 
+	private:
 		DLRuntimeClass* m_pParentClass;
 		DLMethod* m_pConstructor;
 		MethodList m_methods;
 
+	public:
 		DLRuntimeClass();
+
+		virtual ~DLRuntimeClass() {}
+		virtual const dl_char* GetName() const = 0;
+		virtual const dl_wchar* GetUnicodeName() const = 0;
+		// Map the others...
 
 		void AddMethod(DLMethodInvoker<DLMethodInvokeContext>* pMethodInvoker, const dl_char* methodName, const dl_wchar* wMethodName);
 		DLMethod* GetMethod(const dl_char* methodName);
+
+		dl_bool IsOfType(const dl_char* typeName) const
+		{
+			if (strcmp(this->GetName(), typeName) == 0)
+				return true;
+
+			return false;
+		}
+
+		dl_bool IsOfType(const dl_wchar* typeName) const
+		{
+			if (wcscmp(this->GetUnicodeName(), typeName) == 0)
+				return true;
+
+			return false;
+		}
 	};
 }
