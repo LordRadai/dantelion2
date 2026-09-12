3c33ba78fbb881645a886555852d4ba19aab2276
Radai <lordradai@null.net>
2026-09-12 13:24:53 +0200

    Update DLRandomGeneratoXorshift.h

--- dantelion2/dantelion2/Core/Util/detail/DLRandomGeneratoXorshift.h
+++ dantelion2/dantelion2/Core/Util/detail/DLRandomGeneratoXorshift.h
@@ -12,30 +12,30 @@ namespace DLUTD
         dl_uint Next()
         {
             dl_uint t = m_X ^ (m_X << 11);
+
             m_X = m_Y;
             m_Y = m_Z;
             m_Z = m_W;
             m_W = m_W ^ (m_W >> 19) ^ t ^ (t >> 8);
+
             return m_W;
         }
 
         dl_int GetRandomInt(dl_int min, dl_int max)
         {
             dl_uint r = Next();
-            return min + (dl_int)(r % (dl_uint)(max - min + 1));
+
+            return min + static_cast<dl_int>(r % static_cast<dl_uint>(max - min + 1));
         }
 
         dl_float32 GetRandomFloat(dl_float32 min, dl_float32 max)
         {
             dl_uint r = Next();
-            dl_float32 t = (dl_float32)r / (dl_float32)0xFFFFFFFFu;
+            dl_float32 t = static_cast<dl_float32>(r) / static_cast<dl_float32>(UINT_MAX);
+
             return min + t * (max - min);
         }
 
-		dl_uint X() const { return m_X; }
-		dl_uint Y() const { return m_Y; }
-		dl_uint Z() const { return m_Z; }
-		dl_uint W() const { return m_W; }
 	private:
 		dl_uint m_X;
 		dl_uint m_Y;
